2800d9b6461cc4fe6b5fbb579482c1f2ba0e4ccd
BabbyBrown03 <revaldousskuncoroputra@gmail.com>
2025-11-06 13:46:21 +0700

    setup email config menggunakan Gmail

---
+++ app/Http/Controllers/SendEmailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmail;
+
+class SendEmailController extends Controller
+{
+    public function index()
+ {
+ $content = [
+ 'name' => 'Ini Nama Pengirim',
+ 'subject' => 'Ini subject email',
+ 'body' => 'Ini adalah isi email yang
+dikirim dari laravel 10'
+ ];
+Mail::to('revaldousskuncoroputra@gmail.com')->send(new SendEmail($content));
+ return "Email berhasil dikirim.";
+ }
+}
